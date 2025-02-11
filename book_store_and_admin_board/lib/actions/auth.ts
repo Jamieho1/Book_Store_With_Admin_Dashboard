70d37c9bfcf0f796f62b738da6769dbd90c63514
jamieho1 <shinghimho@gmail.com>
2025-02-11 11:41:54 +0000

    commit 13: 1) trigger onboarding workflow since from sign up, 2) setup resend token , 3) after hook updating user lastActivityDate 4) workflow define "active", delay time and content setup,5) workflow email subject and message setup

--- book_store_and_admin_board/lib/actions/auth.ts
+++ book_store_and_admin_board/lib/actions/auth.ts
@@ -1,16 +1,18 @@
 "use server";
 
-import { users } from "@/database/schema";
 import { eq } from "drizzle-orm";
 import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
 import { headers } from "next/headers";
 import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "@/lib/actions/workflow";
+import config from "@/lib/config";
 
 export const signInWithCredentials = async (
-  params: Pick<AuthCredentials, "email" | "password">,
+    params: Pick<AuthCredentials, "email" | "password">,
 ) => {
   const { email, password } = params;
 
@@ -25,9 +27,11 @@ export const signInWithCredentials = async (
       password,
       redirect: false,
     });
+
     if (result?.error) {
       return { success: false, error: result.error };
     }
+
     return { success: true };
   } catch (error) {
     console.log(error, "Signin error");
@@ -44,14 +48,17 @@ export const signUp = async (params: AuthCredentials) => {
   if (!success) return redirect("/too-fast");
 
   const existingUser = await db
-    .select()
-    .from(users)
-    .where(eq(users.email, email));
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
 
   if (existingUser.length > 0) {
     return { success: false, error: "User already exists" };
   }
+
   const hashedPassword = await hash(password, 10);
+
   try {
     await db.insert(users).values({
       fullName,
@@ -60,6 +67,17 @@ export const signUp = async (params: AuthCredentials) => {
       password: hashedPassword,
       universityCard,
     });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email,
+        fullName,
+      },
+    });
+
+    await signInWithCredentials({ email, password });
+
     return { success: true };
   } catch (error) {
     console.log(error, "Signup error");
